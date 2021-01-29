9b1ce698e156ac8cfdf627313103fc0de18e12ce
ginakui <developerkui@outlook.com>
2021-01-28 22:25:07 -0500

    refactor configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,18 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import thunk from 'redux-thunk';
-//import expensesReducer from '../reducers/expenses';
 import expensesReducer from '../slices/expensesSlice';
-//import filtersReducer from '../reducers/filters';
 import filtersReducer from '../slices/filtersSlice'
-//import authReducer from '../reducers/auth';
 import authReducer from '../slices/authSlice';
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-/**
- * @TODO refactor redux store to use redux toolkit
- */
 export default () => {
   const store = configureStore({
     reducer: {
