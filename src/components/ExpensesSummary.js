c637cfbf29c9fb394888cb663ae6d5604c0a706d
ginakui <developerkui@outlook.com>
2021-01-29 20:59:43 -0500

    refactor ExpensesSummary using useSelector hooks

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
-	const expenseWord = expenseCount <= 1 ? 'expense':'expenses';
-	const formattedExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
+ const ExpensesSummary = () => {
+	const filteredExpenses = useSelector(selectExpenses);
+	const totalExpenses = selectExpensesTotal(filteredExpenses);
+	const expenseWord = filteredExpenses.length <= 1 ? 'expense':'expenses';
+	const formattedExpensesTotal = numeral(totalExpenses/100).format('$0,0.00');
 	return (
 		<div className="page-header">
 			<div className="content-container">
-				<h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totaling <span>{formattedExpensesTotal}</span></h1>			
+				<h1 className="page-header__title">Viewing <span>{filteredExpenses.length}</span> {expenseWord} totaling <span>{formattedExpensesTotal}</span></h1>			
 				<div className="page-header__actions">
 					<Link className="button" to="/create">Add Expense</Link>
 				</div>
@@ -20,12 +22,5 @@ export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
 	);
 };
 
-const mapStateToProps = (state) => {
-	const visibleExpenses = selectExpenses(state.expenses, state.filters);
-	return {
-		expenseCount: visibleExpenses.length,
-		expensesTotal: selectExpensesTotal(visibleExpenses)
-	};
-};
-
-export default connect(mapStateToProps)(ExpensesSummary);
+//export default connect(mapStateToProps)(ExpensesSummary);
+export default ExpensesSummary;
